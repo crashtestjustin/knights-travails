384ddc3e5c23981d5e20d5ac5810eb42a9ae2080
Justin Elliott <thecrashtestjustin@gmail.com>
2023-05-10 07:09:49 -0700

    created legal moves function
    created funtion that takes in current position of knight and returns
    an array of the new possible positions of the knight based on legal moves
    on an 8x8 board

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 import { boardCreate, Knight } from "./gameboard";
+
+function knightMoves(start, end) {
+  const board = boardCreate();
+  const knight = new Knight(start[0], start[1]);
+  legalMoves(start[0], start[1]);
+}
+
+function legalMoves(col, row) {
+  const possibleMoves = [
+    [1, -2],
+    [2, -1],
+    [2, 1],
+    [1, 2],
+    [2, -1],
+    [-1, 2],
+    [-2, 1],
+    [-2, -1],
+    [-1, -2],
+  ];
+
+  const validMoves = [];
+
+  possibleMoves.forEach((move) => {
+    const newCol = col + move[0];
+    const newRow = row + move[1];
+    if (
+      row + move[1] >= 0 &&
+      row + move[1] <= 7 &&
+      col + move[0] >= 0 &&
+      col + move[0] <= 7
+    ) {
+      validMoves.push([newCol, newRow]);
+    }
+  });
+  return validMoves;
+}
+console.log(legalMoves(1, 3));
