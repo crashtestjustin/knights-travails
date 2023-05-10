4d61fecae3879b4716f2e210eecfe67d36fd7270
Justin Elliott <thecrashtestjustin@gmail.com>
2023-05-10 06:28:13 -0700

    created functions that create a board & knight

---
+++ gameboard.js
@@ -0,0 +1,23 @@
+export function boardCreate() {
+  const rows = 8;
+  const cols = 8;
+
+  const board = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
+  return board;
+}
+
+console.log(boardCreate());
+
+export class Knight {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
+
+  getPosition() {
+    return [this.row, this.col];
+  }
+}
+
+const knight = new Knight(0, 0);
+console.log(knight.getPosition());
---
+++ script.js
@@ -0,0 +1,1 @@
+import { boardCreate, Knight } from "./gameboard";
